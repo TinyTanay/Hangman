38bb70012cb58df6c99245a8830da96fec81db34
tanay.kulshreshtha <tanay.kuls@gmail.com>
2021-11-03 14:01:57 +1100

    added word completion.

--- Hangman.py
+++ Hangman.py
@@ -6,3 +6,7 @@ word_lst = ['opal', 'python', 'jobs', 'random2']
 def get_word(word_lst):
     word = random.choice(word_lst)
     return word
+
+def play(word):
+    word_completion = '_' * len(word)
+    
