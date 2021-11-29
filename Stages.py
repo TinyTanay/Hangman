fdd3642f029ef86b466de6e23505a9ada3493f37
TinyTanay <Tanay.kuls@gmail.com>
2021-11-29 23:15:15 +1100

    Keeps the pictures on screen for a bit.

--- Stages.py
+++ Stages.py
@@ -8,6 +8,7 @@ def stages(tries):
     window.setup(width=800, height=600, startx=1100, starty=0)
     turtle.pensize(15)
     if tries == 1:
+        turtle.clear()
         global running
         turtle.circle(50)
         turtle.right(45)
@@ -23,9 +24,10 @@ def stages(tries):
         turtle.backward(100)
         turtle.left(90)
         turtle.forward(100)
-        sleep(1)
+        sleep(5)
         turtle.reset()
     elif tries == 2:
+        turtle.clear()
         global running
         turtle.circle(50)
         turtle.right(45)
@@ -38,9 +40,10 @@ def stages(tries):
         turtle.forward(200)
         turtle.right(45)
         turtle.forward(100)
-        sleep(1)
+        sleep(5)
         turtle.reset()
     elif tries == 3:
+        turtle.clear()
         global running
         turtle.circle(50)
         turtle.right(45)
@@ -51,9 +54,10 @@ def stages(tries):
         turtle.backward(150)
         turtle.left(45)
         turtle.forward(200)
-        sleep(1)
+        sleep(5)
         turtle.reset()
     elif tries == 4:
+        turtle.clear()
         global running
         turtle.circle(50)
         turtle.right(45)
@@ -61,21 +65,24 @@ def stages(tries):
         turtle.backward(150)
         turtle.right(45)
         turtle.forward(150)
-        sleep(1)
+        sleep(5)
         turtle.reset()
     elif tries == 5:
+        turtle.clear()
         global running
         turtle.circle(50)
         turtle.right(90)
         turtle.forward(150)
-        sleep(1)
+        sleep(5)
         turtle.reset()
     elif tries == 6:
+        turtle.clear()
         global running
         turtle.circle(50)
-        sleep(1)
+        sleep(5)
         turtle.reset()
     elif tries == 100:
+        turtle.clear()
         global running
         style = ('Courier', 30, 'italic')
         turtle.write('POSITION THIS', font=style, align='center')
